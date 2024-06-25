f332eda1c0d44ab66a3c775796fd26ffb2632e4d
adminMngEnvMCAP157851 <120586434+adminMngEnvMCAP157851@users.noreply.github.com>
2024-06-25 11:27:46 -0600

    updates.

---
+++ PowerShell/PowerPlatform-Utility.ps1
@@ -0,0 +1,41 @@
+function Install-Pac-Cli{
+	param(
+        [Parameter()] [String]$nugetPackageVersion		
+	)
+    $nugetPackage = "Microsoft.PowerApps.CLI"
+    $outFolder = "pac"
+    if($nugetPackageVersion -ne '') {
+        nuget install $nugetPackage -Version $nugetPackageVersion -OutputDirectory $outFolder
+    }
+    else {
+        nuget install $nugetPackage -OutputDirectory $outFolder
+    }
+    $pacNugetFolder = Get-ChildItem $outFolder | Where-Object {$_.Name -match $nugetPackage + "."}
+    $pacPath = $pacNugetFolder.FullName + "\tools"
+    Write-Host "##vso[task.setvariable variable=pacPath]$pacPath"	
+}
+<#
+This function copies the generated package deployer file (i.e., pdpkg.zip).
+Moves the file to ReleaseAssets folder.
+#>
+function Copy-Pdpkg-File{
+    param (
+        [Parameter(Mandatory)] [String]$appSourcePackageProjectPath,
+        [Parameter(Mandatory)] [String]$packageFileName,
+        [Parameter(Mandatory)] [String]$appSourceAssetsPath,
+        [Parameter(Mandatory)] [String]$binPath
+    )
+
+    Write-Host "pdpkg file found under $appSourcePackageProjectPath\$binPath"
+    Write-Host "Copying pdpkg.zip file to $appSourceAssetsPath\$packageFileName"
+            
+    Get-ChildItem "$appSourcePackageProjectPath\$binPath" -Filter *pdpkg.zip | Copy-Item -Destination "$appSourceAssetsPath\$packageFileName" -Force -PassThru
+    # Copy pdpkg.zip file to ReleaseAssets folder
+    if(Test-Path "$releaseAssetsDirectory"){
+        Write-Host "Copying pdpkg file to Release Assets Directory"
+        Get-ChildItem "$appSourcePackageProjectPath\$binPath" -Filter *pdpkg.zip | Copy-Item -Destination "$releaseAssetsDirectory" -Force -PassThru
+    }
+    else{
+        Write-Host "Release Assets Directory is unavailable to copy pdpkg file; Path - $releaseAssetsDirectory"
+    }
+}
